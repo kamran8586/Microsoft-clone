e3f92ac40f6bbb297a838194a933df6bb83bf66a
Kamran <muhammadkamran8586@gmail.com>
2022-02-22 07:08:40 +0500

    formet code with prettier and add menu bar button

--- controle/app.js
+++ controle/app.js
@@ -1,13 +1,27 @@
-const openAllMicrosoft = document.querySelector('.show-all-microsoft');
-const ModalContainer = document.querySelector('.modal-container');
-
-openAllMicrosoft.addEventListener('click', function () {
-    ModalContainer.classList.toggle('show-modal');
-    ModalContainer.classList.toggle('hidden');
+const openAllMicrosoft = document.querySelector(".show-all-microsoft");
+const ModalContainer = document.querySelector(".modal-container");
+const menuBar = document.getElementById("menuBar");
+const closeMenuBar = document.querySelector(".closeMenuBar");
+const openMenuBar = document.querySelector(".openMenuBar");
+let children = document.querySelector(".parent").children;
+const closeIconClasses = ["fa", "fa-times"];
+const menuIconClasses = ["fa-solid", " fa-bars"];
+openAllMicrosoft.addEventListener("click", (event) => showModal());
+menuBar.addEventListener("click", (event) => {
+	showModal();
+	menuBar.classList.add("hidden");
+	closeMenuBar.classList.remove("hidden");
 });
 
-ModalContainer.addEventListener('click', function (event) {
-    if (event.target != this) return;
-    ModalContainer.classList.remove('show-modal');
-    ModalContainer.classList.add('hidden');
+ModalContainer.addEventListener("click", function (event) {
+	if (event.target != this) return;
+	ModalContainer.classList.remove("show-modal");
+	ModalContainer.classList.add("hidden");
+	closeMenuBar.classList.add("hidden");
+	menuBar.classList.remove("hidden");
 });
+
+function showModal() {
+	ModalContainer.classList.toggle("show-modal");
+	ModalContainer.classList.toggle("hidden");
+}
